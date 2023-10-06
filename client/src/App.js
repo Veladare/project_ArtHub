8198599915525130b603fba208296f5d2a98f0eb
Jean-Luc <jeanlucv@berkeley.edu>
2023-10-05 19:57:30 -0700

    Removed unnesssary code

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
-import SingleThought from './pages/SingleThought';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -59,9 +58,7 @@ function App() {
                 path="/signup" 
                 element={<Signup />} 
               />
-              <Route 
-                path="/thoughts/:thoughtId" 
-                element={<SingleThought />} 
+              <Route
               />
             </Routes>
           </div>
