7c4034b1822a29c39d27052f38f34000b8cfc9cc
Jared Buttrey <geraldomania@hotmail.com>
2023-10-10 20:44:43 -0700

    updates to art file paths

--- client/src/components/artPortfolio.js
+++ client/src/components/artPortfolio.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Art from './Art';
 import './artPortfolio.css';//if we have css need to import it here
 
-import artPiece from './artPiece';//will need img folders for all these
-import artPiece2 from './artPiece2';
-import artPiece3 from './artPiece3';
-import artPiece4 from './artPiece4';
-import artPiece5 from './artPiece5';
-import artPiece6 from './artPiece6';
+import artPiece from '../../../server/assets/David.jpeg';//might need to fix these paths
+import artPiece2 from '../../../server/assets/Guernica.jpeg';
+import artPiece3 from '../../../server/assets/Jimson_Weed.jpeg';
+import artPiece4 from '../../../server/assets/Mona_Lisa.jpeg';
+import artPiece5 from '../../../server/assets/number-1a.jpeg';
+import artPiece6 from '../../../server/assets/Starry_Night.jpeg';
 
 const artPieces = [
     {image: artPiece, altText: "artPiece", description: "artPiece"},
