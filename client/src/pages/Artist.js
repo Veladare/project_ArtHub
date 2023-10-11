b75b46689799d64ed504e1931fd6d80f844b8cce
Jean-Luc <jeanlucv@berkeley.edu>
2023-10-11 12:10:58 -0700

    Fixed query

--- client/src/pages/Artist.js
+++ client/src/pages/Artist.js
@@ -3,18 +3,19 @@ import { useQuery } from '@apollo/client';
 import { QUERY_ARTIST } from '../utils/queries';
 import { useParams } from 'react-router-dom';
 
+
 const Artist = () => {
   const { artistId } = useParams();
 
   // Use the useQuery hook to execute the query
   const { loading, error, data } = useQuery(QUERY_ARTIST, {
-    variables: { artistId: artistId }, 
+    variables: { artistId: artistId },
   });
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;;
+  if (error) return <p>Error: {error.message}</p>;
 
-  const artist = data.artist;
+  const artist = data?.artist || {};
 
   return (
     <main>
@@ -23,15 +24,9 @@ const Artist = () => {
           className="col-12 col-md-10 mb-3 p-3"
           style={{ border: '1px dotted #1a1a1a' }}
         >
-          {/* Artist Name */}
           <h1>{artist.name}</h1>
-
-          {/* Artist Style */}
-          <h2>Art Style: {artist.style}</h2>
-          {/* Artist Bio */}
-          <p>{artist.bio}</p>
-          {/* Art Photo */}
-
+          <p>Style: {artist.style}</p>
+          <p>Bio: {artist.bio}</p>
         </div>
       </div>
     </main>
