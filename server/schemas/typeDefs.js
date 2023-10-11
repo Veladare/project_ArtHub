968c5ab6f72de7bf950d7f3dd979617d416c0e24
Jean-Luc <jeanlucv@berkeley.edu>
2023-10-10 19:22:11 -0700

    Fixed

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,7 +5,7 @@ const typeDefs = gql`
     _id: ID
     name: String
     artist: Artist
-    added: Date
+    added: Int
     medium: String
     description: String
   }
@@ -33,21 +33,11 @@ const typeDefs = gql`
   type Query {
     users: [User]
     user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(thoughtId: ID!): Thought
   }
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    addThought(thoughtText: String!, thoughtAuthor: String!): Thought
-    addComment(
-      thoughtId: ID!
-      commentText: String!
-      commentAuthor: String!
-    ): Thought
-    removeThought(thoughtId: ID!): Thought
-    removeComment(thoughtId: ID!, commentId: ID!): Thought
   }
 `;
 
