34a84fa6e130c303b1e2f992f294b5e90bee8e56
Jean-Luc <jeanlucv@berkeley.edu>
2023-10-10 20:44:35 -0700

    Added Query_ARIST

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -16,3 +16,15 @@ export const QUERY_USER = gql`
 `;
 
 
+export const QUERY_ARTIST = gql`
+  query artist($artistId: ID!) {
+    artist(_id: $artistId) {
+      name
+      style
+      bio
+      art {
+        _id
+      }
+    }
+  }
+`;
