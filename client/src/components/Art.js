672c74235f732a37bfd4a7933eaff18916dcad16
Jared Buttrey <geraldomania@hotmail.com>
2023-10-12 20:52:53 -0700

    artist pics to render

--- client/src/components/Art.js
+++ client/src/components/Art.js
@@ -7,6 +7,10 @@ import artPiece4 from '../assets/Art/Mona_Lisa.jpeg';
 import artPiece5 from '../assets/Art/number-1a.jpeg';
 import artPiece6 from '../assets/Art/Starry_Night.jpeg';
 import artPiece7 from '../assets/Art/the_persistence_of_memory.jpeg';
+import artPiece8 from '../assets/Art/the_thinker.jpeg';
+import artPiece9 from '../assets/Art/the_two_fridas.jpeg';
+import artPiece10 from '../assets/Art/Water_Lilies.jpeg';
+
 
 const artPieces = [
     {image: artPiece, altText: "artPiece", description: "artPiece", title: "Starry Night"},
@@ -16,7 +20,11 @@ const artPieces = [
     {image: artPiece5, altText: "artPiece5", description: "artPiece5"},
     {image: artPiece6, altText: "artPiece6", description: "artPiece6"},
     {image: artPiece7, altText: "artPiece7", description: "artPiece7"},
+    {image: artPiece8, altText: "artPiece8", description: "artPiece8"},
+    {image: artPiece9, altText: "artPiece9", description: "artPiece9"},
+    {image: artPiece10, altText: "artPiece10", description: "artPiece10"},
 ];
+
 
 function Art({ title, description }) {
     const artImage = artPieces.find((art) => art.title === title);
