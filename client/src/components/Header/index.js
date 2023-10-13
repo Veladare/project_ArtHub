a389baa34cbb6849a2c830a85d62e6a7ee589dfd
octofoxx <devilmaycry87x@gmail.com>
2023-10-12 18:39:13 -0700

    re=arranged header layout

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -15,14 +15,14 @@ const Header = () => {
   return (
     <header className=" mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
+      <div style={{paddingLeft:'50px', paddingRight:'50px'}}>
+          <img src={Logo} alt='Logo' style={{height:'100px', width:'150px'}}/>
+        </div>
         <div>
           <Link to="/">
             <h1 className="m-0">Berkeley Art Museum</h1>
           </Link>
           <p className="m-0">Browse the gallery of our featured artists!</p>
-        </div>
-        <div style={{paddingLeft:'50px', paddingRight:'50px'}}>
-          <img src={Logo} alt='Logo' style={{height:'200px', width:'150px'}}/>
         </div>
         <div>
           {Auth.loggedIn() ? (
